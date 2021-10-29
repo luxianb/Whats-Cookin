ef5640c5e706995ef621b0f9dc196018ae956383
luxianb <40238895+luxianb@users.noreply.github.com>
2021-10-29 17:54:21 +0800

    Edit server.js file

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const session = require('express-session')
+
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT ?? 3002;
+mongoose.connect(process.env.MONGODB_URI ?? 'mongodb://localhost:27017/whatsCookin')
+mongoose.connection.on("open", () => {
+  console.log(`Connection to MongoDB ${process.env.MONGODB_URI ? "Atlas" : ""} is open`)
+})
+
+// Middleware
+app.use(express.json())
+app.use(session({ secret: process.env.SECRET, resave: false, saveUninitialized: false }))
+
+// Routes
+const sessionController = require('./controllers/session_controller');
+app.use('/api/session', sessionController)
+const userController = require('./controllers/user');
+app.use('/api/user', userController)
+
+app.get('/', (req, res) => {
+  res.send("Hello world")
+})
+
+// Listerner
+app.listen(port, () => {
+  console.log(`Express server is live at ${port}`)
+})
