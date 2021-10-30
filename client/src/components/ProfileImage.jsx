bb9bb2a30547d2b9379f4db0587c098ddf1d3e7e
luxianb <40238895+luxianb@users.noreply.github.com>
2021-10-30 18:13:03 +0800

    Component for profile image display

---
+++ client/src/components/ProfileImage.jsx
@@ -0,0 +1,29 @@
+import styled from "styled-components"
+import profileIcon from '../assets/profileIcon.svg'
+
+const ImageContainer = styled.div`
+  height: ${(props) => props.size || `50px`};
+  width: ${(props) => props.size || `50px`};
+  border-radius: 1000px;
+  overflow: hidden;
+`;
+
+const NullPicBackground = styled.div`
+  background-color: rgba(0,0,0,0.1);
+  height: 100%;
+  width: 100%;
+`;
+
+export default function ProfileImage(props) {
+  return(
+    <ImageContainer size={props.size} style={props.style}>
+      {typeof props.src === 'undefined' ? (
+        <NullPicBackground>
+          <img src={profileIcon} alt="" style={{height: '100%', width: '100%'}}/>
+        </NullPicBackground>
+      ) : (
+        <img src={props.src} alt=""/>
+      )}
+    </ImageContainer>
+  )
+}
