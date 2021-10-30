a4ee1ffb4a5d145fd3fa9a120d421600e1c469a0
luxianb <40238895+luxianb@users.noreply.github.com>
2021-10-30 18:13:14 +0800

    Button components

---
+++ client/src/components/Buttons/index.jsx
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+/** Base styling for all buttons */
+const ButtonBase = styled.button`
+  padding: 9px 12px;
+  border-radius: ${(prop) => prop.rounded ? '6px' : "initial"};
+  border: 0;
+  box-sizing: border-box;
+  font-size: .8rem;
+  cursor: pointer;
+  :hover{
+    opacity: .8;
+  }
+`
+
+const PrimaryButton = styled(ButtonBase)`
+  background-color: ${(prop) => prop.color || 'black'};
+  color: white;
+`
+
+const AltButton = styled(ButtonBase)`
+  background-color: ${(prop) => prop.color || '#FFB800'};
+  color: 'black';
+`
+
+const GhostButton = styled(ButtonBase)`
+  box-sizing: border-box;
+  background-color: transparent;
+  border: 1px solid ${(prop) => prop.color || '#FFB800'};
+  color: ${(prop) => prop.color || '#FFB800' };
+`
+
+const Button = {
+  Primary: PrimaryButton,
+  Alt: AltButton,
+  Ghost: GhostButton,
+};
+
+export default Button;
