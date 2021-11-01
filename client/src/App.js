4ffcd7ffa8789b50528515fcf2ac06f15c8a38aa
luxianb <40238895+luxianb@users.noreply.github.com>
2021-11-01 16:53:01 +0800

    Add routes to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
+import { Route, Switch } from "react-router-dom";
+import "./App.css"
+import LogIn from "./pages/LogIn";
+import NavigationBar from './components/NavigationBar'
+
 function App() {
   return (
-    <div>
-
+    <div style={{backgroundColor: 'white'}}>
+      <NavigationBar />
+      <Switch>
+        <Route exact path="/">
+          <div><h1>Hello World</h1></div>
+        </Route>
+        <Route path='/login' component={LogIn}/>
+      </Switch>
     </div>
   );
 }
