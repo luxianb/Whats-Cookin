d5de10d37a8bb16afbe12d494691ea7c24458a44
Perry-E <perryeber@gmail.com>
2021-11-01 21:29:49 +0800

    Added upload route

--- controllers/image.js
+++ controllers/image.js
@@ -3,12 +3,13 @@ const cloudinary = require("../imageUtility/cloudinary");
 const upload = require("../imageUtility/multer");
 const Image = require("../models/image");
 
-router.post("/", upload.single("image"), async (req, res) => {
+//* Upload image
+router.post("/", upload.single("avatar"), async (req, res) => {
     console.log("req.file", req.file);
   try {
     // Upload image to cloudinary
-    const result = await cloudinary.uploader.upload(req.file);
-
+    const result = await cloudinary.uploader.upload(req.file.path);
+    console.log("result", result)
     // Create new image
     let image = new Image({
       name: req.body.name,
@@ -23,59 +24,5 @@ router.post("/", upload.single("image"), async (req, res) => {
   }
 });
 
-router.get("/", async (req, res) => {
-  try {
-    let image = await Image.find();
-    res.json(image);
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-router.delete("/:id", async (req, res) => {
-  try {
-    // Find image by id
-    let image = await Image.findById(req.params.id);
-    // Delete image from cloudinary
-    await cloudinary.uploader.destroy(image.cloudinary_id);
-    // Delete image from db
-    await image.remove();
-    res.json(image);
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-router.put("/:id", upload.single("image"), async (req, res) => {
-  try {
-    let image = await Image.findById(req.params.id);
-    // Delete image from cloudinary
-    await cloudinary.uploader.destroy(image.cloudinary_id);
-    // Upload image to cloudinary
-    let result;
-    if (req.file) {
-      result = await cloudinary.uploader.upload(req.file.path);
-    }
-    const data = {
-      name: req.body.name || image.name,
-      avatar: result?.secure_url || image.avatar,
-      cloudinary_id: result?.public_id || image.cloudinary_id,
-    };
-    image = await Image.findByIdAndUpdate(req.params.id, data, { new: true });
-    res.json(image);
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-router.get("/:id", async (req, res) => {
-  try {
-    // Find image by id
-    let image = await Image.findById(req.params.id);
-    res.json(image);
-  } catch (err) {
-    console.log(err);
-  }
-});
 
 module.exports = router;
