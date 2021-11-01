b31e6f46d06f87aab10eaad4829de787b08703cd
Perry-E <perryeber@gmail.com>
2021-11-01 21:36:42 +0800

    Added update route

--- controllers/image.js
+++ controllers/image.js
@@ -49,6 +49,29 @@ router.delete("/:id", async (req, res) => {
   }
 });
 
+//* Update
+router.put("/:id", upload.single("avatar"), async (req, res) => {
+  try {
+    let image = await Image.findById(req.params.id);
+    // Delete image from cloudinary
+    await cloudinary.uploader.destroy(image.cloudinary_id);
+    // Upload image to cloudinary
+    let result;
+    if (req.file) {
+      result = await cloudinary.uploader.upload(req.file.path);
+    }
+    const data = {
+      name: req.body.name || image.name,
+      avatar: result?.secure_url || image.avatar,
+      cloudinary_id: result?.public_id || image.cloudinary_id,
+    };
+    image = await Image.findByIdAndUpdate(req.params.id, data, { new: true });
+    res.json(image);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 
 
 module.exports = router;
