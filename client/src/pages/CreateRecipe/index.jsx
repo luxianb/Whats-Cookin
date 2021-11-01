12c4f3cd6ff57b0d262c61f6cf17a4ab69367c4f
aFar1s <89140460+aFar1s@users.noreply.github.com>
2021-11-01 15:53:48 +0800

    pages/create input fields

--- client/src/pages/CreateRecipe/index.jsx
+++ client/src/pages/CreateRecipe/index.jsx
@@ -1,10 +1,43 @@
 import React from 'react'
+import { Col, Row } from '../../components/Containers/Index'
 
 const CreateRecipe = () => {
     return (
         <div>
-            <h1>Create</h1>
-            {/* <img src={'.../assets/Rectangle.png'} alt="img"/> */}
+            
+          <h1>Add a Recipe</h1>
+          <Row>
+          
+          <Col>
+             <input type="text" name="fname" placeholder="Meal Name"/>
+             <input type="text" name="fname" placeholder="Description"/>
+             <input type="text" name="fname" placeholder="Tags"/>
+             <span>Est. Time to cook</span>
+            <Row>
+             <input type="text" name="fname" placeholder="hours"/>
+             <input type="text" name="fname" placeholder="minutes"/>
+            </Row>
+             <span>Ingridients</span>
+            <Row>
+             <input type="text" name="fname" placeholder="amount"/>
+             <input type="text" name="fname" placeholder="unit"/>
+             <input type="text" name="fname" placeholder="name"/>
+            </Row>
+            <div>add fields</div>
+            <h3>Steps</h3>
+            <input type="text" name="fname" placeholder="Step title"/>
+            <input type="text" name="fname" placeholder="Instructions"/>
+
+          </Col>
+          <Col>
+            
+            {/* <input type="text" id="fname" name="fname" placeholder="Hours"/> */}
+            
+   
+ 
+          </Col>
+          </Row>
+            
         </div>
     )
 }
