5fd65f1eda97dbdb8e742388594f672ad3caae07
aFar1s <89140460+aFar1s@users.noreply.github.com>
2021-11-02 11:21:51 +0800

    create/ handleSubmit

--- client/src/pages/CreateRecipe/index.jsx
+++ client/src/pages/CreateRecipe/index.jsx
@@ -1,17 +1,52 @@
-import { React, useState } from "react";
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
 import { Col, Row } from '../../components/Containers/Index'
+import Button from "../../components/Buttons/index"
 
 const CreateRecipe = () => {
-    const [createInputMealName, setCreateInputMealName] = useState("")
-    const [createInputDescription, setCreateInputDescription] = useState("")
-    const [createInputTags, setCreateInputTags] = useState("")
-    const [createInputHours, setCreateInputHours] = useState("")
-    const [createInputMinutes, setCreateInputMinutes] = useState("")
-    const [createInputAmount, setCreateInputAmoun] = useState("")
-    const [createInputUnit, setCreateInputUnit] = useState("")
-    const [createInputName, setCreateInputName] = useState("")
-    const [createInputStepTittle, setCreateInputStepTittle] = useState("")
-    const [createInputInstructions, setCreateInputInstructions] = useState("")
+    const [mealName, setMealName] = useState("")
+    const [description, setDescription] = useState("")
+    const [tags, setTags] = useState("")
+    const [hours, setHours] = useState("")
+    const [minutes, setMinutes] = useState("")
+    const [amount, setAmount] = useState("")
+    const [unit, setUnit] = useState("")
+    const [ingridientName, setIngridientName] = useState("")
+    const [stepTittle, setStepTittle] = useState("")
+    const [instructions, setInstructions] = useState("")
+
+    const mealNameUpdate = (event) => {
+        setMealName(event.target.value)
+        // setDescription(event.target.value)
+        // setTags(event.target.value)
+        // setHours(event.target.value)
+        // setMinutes(event.target.value)
+        // setAmount(event.target.value)
+        // setUnit(event.target.value)
+        // setIngridientName(event.target.value)
+        // setStepTittle(event.target.value)
+        // setInstructions(event.target.value)
+    }
+
+    const handleSubmit=()=> { 
+        const postURL = "mongodb://localhost:27017/whatsCookin" 
+        fetch(postURL, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ 
+                name: mealName,
+                clockedIn:false,
+                dates:[]
+            })
+        })
+        .then(()=>{
+            // Once posted, the user will be notified 
+            alert('You have been added to the system!');
+        })
+    }
   
     return (
         <div>
@@ -20,7 +55,8 @@ const CreateRecipe = () => {
           <Row>
           
           <Col>
-             <input type="text" className="input-text-normal" placeholder="Meal Name"/>
+            <form>
+             <input required onChange={mealNameUpdate} type="text" className="input-text-normal" placeholder="Meal Name"/>
              <input type="text" className="input-text-large" placeholder="Description"/>
              <input type="text" className="input-text-normal" placeholder="Tags"/>
              <span>Est. Time to cook</span>
@@ -35,20 +71,18 @@ const CreateRecipe = () => {
              <input type="text" className="input-text-normal" placeholder="name"/>
             </Row>
             <div>add fields</div>
-            <h3>Steps</h3>
-            <input type="text" className="input-text-normal" placeholder="Step title"/>
-            <input type="text" className="input-text-large" placeholder="Instructions"/>
-
+             <h3>Steps</h3>
+             <input type="text" className="input-text-normal" placeholder="Step title"/>
+             <input type="text" className="input-text-large" placeholder="Instructions"/>
+            <Button.Ghost>
+             <button type="submit"> Submit</button>
+            </Button.Ghost>
+            </form>
           </Col>
           <Col>
-            
             {/* <input type="text" id="fname" name="fname" placeholder="Hours"/> */}
-            
-   
- 
           </Col>
           </Row>
-            
         </div>
     )
 }
