e6e0ba8eab4d15095a118ac58133d45215ade2e9
Perry-E <perryeber@gmail.com>
2021-11-02 15:02:11 +0800

    Added recipe cards for meal preview

--- client/src/pages/Landing/index.jsx
+++ client/src/pages/Landing/index.jsx
@@ -1,40 +1,83 @@
 import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const Landing = () => {
+  const [card, setCard] = useState();
+
+  useEffect(() => {
+    async function fetchRecipeData() {
+      const res = await axios.get("/api/recipes");
+      setCard(res.data);
+    }
+    fetchRecipeData();
+  }, []);
+  console.log("card", card);
+
   return (
     <>
-    <div className="landing-div">
-      <div>
-        <h1 className="landing">Landing Image</h1>
-      </div>
-      <div>
-        <h3 className="landing">About Us</h3>
-        <p className="landing">
-          Stuck at home coding and thinking about your next fancy meal? Well
-          look no further because What's Cookin is your go to service provider
-          that offers you a wide array of recipes, from breakfast to dinner.
-          Members of What's Cookin are able to use our service to its fullest
-          potential, so what are you waiting for, sign up today!
-        </p>
-      </div>
-      <div>
-        <h3 className="how">How it works</h3>
-        <div className="bar-container">
-          <div>1</div>
-          <div>2</div>
-          <div>3</div>
-          <div>4</div>
+      <div className="landing-div">
+        <div>
+          <h1 className="landing">Landing Image</h1>
         </div>
-        <div className="bar-container" style={{"margin": "0px"}}>
-          <p>Sign Up as a member!</p>
-          <p>Customise your profile and add <br/> your next meal to the planner!</p>
-          <p>Add your own recipes and reviews!</p>
-          <p>Follow along with our ingredient <br/> checklist and recipe!</p>
+        <div>
+          <h3 className="landing">About Us</h3>
+          <p className="landing">
+            Stuck at home coding and thinking about your next fancy meal? Well
+            look no further because What's Cookin is your go to service provider
+            that offers you a wide array of recipes, from breakfast to dinner!
+            Members of What's Cookin are able to use our service to its fullest
+            potential, so what are you waiting for, sign up today!
+          </p>
         </div>
-        
+        <div>
+          <h3 className="how">How it works</h3>
+          <div className="bar-container">
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+            <div>4</div>
+          </div>
+          <div className="bar-container" style={{ margin: "0px" }}>
+            <p className="how-text1">Sign Up as a member!</p>
+            <p className="how-text2">
+              Customise your profile and add your next meal to the planner!
+            </p>
+            <p className="how-text3">Add your own recipes and reviews!</p>
+            <p className="how-text4">
+              Follow along with our ingredient checklist and recipe!
+            </p>
+          </div>
+        </div>
+        <h3 style={{ "text-align": "left" }}>Meals</h3>
+        <div className="container">
+          {card?.map((item) => {
+            return (
+              <>
+                <div className="card">
+                  <img
+                    src={item.picture}
+                    alt="Avatar"
+                    style={{ width: "100%" }}
+                    className="landing-image"
+                  />
+                  <div className="card-text">
+                    <h4>
+                      <b>{item.name}</b>
+                    </h4>
+                    <p>
+                      {item.time.hour > 0 ? `${item.time.hour} hours` : null}{" "}
+                      {item.time.minutes > 0
+                        ? `${item.time.minutes} mins`
+                        : null}{" "}
+                    </p>
+                  </div>
+                </div>
+              </>
+            );
+          })}
+        </div>
       </div>
-    </div>
-      {/* add their go-to recipes to share with the rest of the community and recieve reviews! Members can also keep track of recipes that they would like to experiement */}
     </>
   );
 };
