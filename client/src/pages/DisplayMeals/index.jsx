0938e84b0ef2845343989a3feeddca6bd1e99bf9
Perry-E <perryeber@gmail.com>
2021-11-02 22:38:17 +0800

    Added states for bkfast, lunch and dinner

---
+++ client/src/pages/DisplayMeals/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Button from "../../components/Buttons/index";
+
+const DisplayMeals = () => {
+  const [card, setCard] = useState();
+  const [breakfast, setBreakfast] = useState();
+  const [lunch, setLunch] = useState();
+  const [dinner, setDinner] = useState();
+
+
+};
+
+export default DisplayMeals;
