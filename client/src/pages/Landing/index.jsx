f6c62c4e9553f36d7fbbf810a0e862f60c692b3c
Perry-E <perryeber@gmail.com>
2021-11-02 17:21:14 +0800

    Adjusted How it works alignment

--- client/src/pages/Landing/index.jsx
+++ client/src/pages/Landing/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import Button from "../../components/Buttons/index"
+import Button from "../../components/Buttons/index";
 
 const Landing = () => {
   const [card, setCard] = useState();
@@ -18,7 +18,7 @@ const Landing = () => {
 
   const handleClick = (event) => {
     console.log("CLICKED", event.target);
-  }
+  };
 
   return (
     <>
@@ -38,62 +38,78 @@ const Landing = () => {
         </div>
         <div>
           <h3 className="how">How it works</h3>
-          <div className="bar-container">
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-            <div>4</div>
+          <div className="how-container">
+            <div className="bar-container">
+              <div className="child">1</div>
+              <p className="how-text">Sign Up as a member!</p>
+            </div>
+
+            <div className="bar-container">
+              <div className="child">2</div>
+              <p className="how-text">
+                Customise your profile and add your next meal to the planner!
+              </p>
+            </div>
+
+            <div className="bar-container">
+              <div className="child">3</div>
+              <p className="how-text">Add your own recipes and reviews!</p>
+            </div>
+
+            <div className="bar-container">
+              <div className="child">4</div>
+              <p className="how-text">
+                Follow along with our ingredient checklist and recipe!
+              </p>
+            </div>
           </div>
-          <div className="bar-container" style={{ margin: "0px" }}>
-            <p className="how-text1">Sign Up as a member!</p>
-            <p className="how-text2">
-              Customise your profile and add your next meal to the planner!
-            </p>
-            <p className="how-text3">Add your own recipes and reviews!</p>
-            <p className="how-text4">
-              Follow along with our ingredient checklist and recipe!
-            </p>
-          </div>
+
+          <div className="bar-container" style={{ margin: "0px" }}></div>
         </div>
-        <h3 style={{ "textAlign": "left" }}>Meals</h3>
+        <h3 style={{ textAlign: "left" }}>Meals</h3>
         <div className="container">
           {card?.map((item) => {
             return (
               <>
-                <div key={item._id} className="card" onClick={handleClick} >
-                    <Link to={`/recipes/${item._id}`} style={{textDecoration: "none", color:"black"}}>
-                  <img
-                    src={item.picture}
-                    alt="Avatar"
-                    style={{ width: "100%" }}
-                    className="landing-image"
-                  />
-                  <div className="card-text" key={item._id}>
-                    <h4>
-                      <b>{item.name}</b>
-                    </h4>
-                    <p>
-                      {item.time.hour > 0 ? `${item.time.hour} hours` : null}{" "}
-                      {item.time.minutes > 0
-                        ? `${item.time.minutes} mins`
-                        : null}{" "}
-                    </p>
-                  </div>
-                        </Link>
+                <div key={item._id} className="card" onClick={handleClick}>
+                  <Link
+                    to={`/recipes/${item._id}`}
+                    style={{ textDecoration: "none", color: "black" }}
+                  >
+                    <img
+                      src={item.picture}
+                      alt="Avatar"
+                      style={{ width: "100%" }}
+                      className="landing-image"
+                    />
+                    <div className="card-text" key={item._id}>
+                      <h4>
+                        <b>{item.name}</b>
+                      </h4>
+                      <p>
+                        {item.time.hour > 0 ? `${item.time.hour} hours` : null}{" "}
+                        {item.time.minutes > 0
+                          ? `${item.time.minutes} mins`
+                          : null}{" "}
+                      </p>
+                    </div>
+                  </Link>
                 </div>
               </>
             );
-        })}
+          })}
         </div>
         <Link to={`/recipes`}>
-        <Button.Ghost style={{borderRadius: "5px"}}>See More</Button.Ghost>
+          <Button.Ghost style={{ borderRadius: "5px" }}>See More</Button.Ghost>
         </Link>
       </div>
       <div className="bottom-signup">
-      <h4>What are you waiting for?</h4>
-      <Link to={`/signup`} style={{textDecoration: "none", color:"black"}}>
-          <Button.Alt style={{borderRadius: "5px"}}>Sign Up!</Button.Alt>
-      </Link>
+        <h4>What are you waiting for?</h4>
+        <Link to={`/signup`} style={{ textDecoration: "none", color: "black" }}>
+          <Button.Alt style={{ borderRadius: "5px", color: "white" }}>
+            Sign Up
+          </Button.Alt>
+        </Link>
       </div>
     </>
   );
