b28d4ba8ce9a84aead64851f64349d647e432a6a
Perry-E <perryeber@gmail.com>
2021-11-02 23:53:33 +0800

    Added allLunch recipe route

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { Route, Switch } from "react-router-dom";
 import "./App.css"
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
+import axios from "axios";
 import LogIn from "./pages/LogIn";
 import NavigationBar from './components/NavigationBar'
 import Landing from "./pages/Landing/index"
@@ -8,21 +9,44 @@ import CreateRecipe from "./pages/CreateRecipe/index"
 import SignUp from "./pages/SignUp";
 import DisplayMeals from "./pages/DisplayMeals";
 import DisplayBreakfast from "./pages/DisplayMeals/BreakfastAll";
+import DisplayLunch from "./pages/DisplayMeals/LunchAll";
 
 function App() {
   const [userData, setUserData] = useState({});
+  const [card, setCard] = useState();
+
+  useEffect(() => {
+    async function fetchRecipeData() {
+      const res = await axios.get("/api/recipes");
+      setCard(res.data);
+    }
+    fetchRecipeData();
+  }, []);
 
   return (
     <div style={{backgroundColor: 'white'}}>
       <NavigationBar userData={userData}/>
       <Switch>
         <Route exact path="/" component={Landing} />
-        <Route exact path="/meals" component={DisplayMeals} />
-        <Route exact path="/meals/breakfast" component={DisplayBreakfast} />
+
+        <Route exact path="/meals" component={DisplayMeals}>
+          <DisplayMeals recipes={card}/>
+        </Route>
+
+        <Route exact path="/meals/breakfast" component={DisplayBreakfast}>
+        <DisplayBreakfast recipes={card}/>
+        </Route>
+
+        <Route exact path="/meals/lunch" component={DisplayLunch}>
+        <DisplayLunch recipes={card}/>
+        </Route>
+
         <Route exact path="/create" component={CreateRecipe} />
+
         <Route path='/login'>
           <LogIn onLoginSuccess={(userInfo) => setUserData(userInfo)}/>
         </Route>
+
         <Route path='/signup' component={SignUp}/>
       </Switch>
     </div>
