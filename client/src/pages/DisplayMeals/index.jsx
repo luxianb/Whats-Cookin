b92dd8bf33a7f5703adaf74e51ef3e08b928a5f2
Perry-E <perryeber@gmail.com>
2021-11-03 19:35:52 +0800

    Created index page for all meals

--- client/src/pages/DisplayMeals/index.jsx
+++ client/src/pages/DisplayMeals/index.jsx
@@ -1,188 +1,45 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
-// import Button from "../../components/Buttons/index";
-import SearchBar from "../../components/SearchBar";
+import SearchBar from "../../components/SearchBar/Index";
 
 const DisplayMeals = (props) => {
   const allMeals = props.recipes;
-  console.log("DisplayAll meals", allMeals);
-  const [card, setCard] = useState();
-//   const [breakfast, setBreakfast] = useState();
-//   const [lunch, setLunch] = useState();
-//   const [dinner, setDinner] = useState();
-
-  useEffect(() => {
-    setCard(allMeals);
-
-    //* Filter by type (Breakfast)
-    // const bkfast = allMeals?.filter((item) => {
-    //   return item.type === "Breakfast";
-    // });
-    // setBreakfast(bkfast);
-
-    //* Filter by type (Lunch)
-    // const lunchData = allMeals?.filter((item) => {
-    //   return item.type === "Lunch";
-    // });
-    // setLunch(lunchData);
-
-    //* Filter by type (Dinner)
-//     const dinnerData = allMeals?.filter((item) => {
-//       return item.type === "Dinner";
-//     });
-//     setDinner(dinnerData);
-  }, [allMeals]);
-
-  //* All recipe data
-  console.log("All cards", card);
-//   console.log("Breakfast", breakfast);
-//   console.log("Lunch", lunch);
-//   console.log("Dinner", dinner);
+  const [filteredMeal, setFilteredMeal] = useState(allMeals);
 
   return (
     <>
       <div className="menuIndex">
-        <h1>Meals</h1>
-
-        {/* <h3 className="mealType">Breakfast</h3> */}
-        <SearchBar/>
+        <h1 style={{marginBottom:"70px", marginTop:"50px"}}>Meals</h1>
+        <SearchBar recipes={allMeals} setFilteredMeal={setFilteredMeal} />
         <div className="container">
-          {card?.map((item) => {
+          {filteredMeal?.map((item) => {
             return (
-              <>
-                <div key={item._id} className="card">
-                  <img
-                    src={item.picture}
-                    alt="Avatar"
-                    style={{ width: "100%" }}
-                    className="landing-image"
-                  />
-                  <div className="card-text" key={item._id}>
-                    <Link
-                      to={`/recipes/${item._id}`}
-                      style={{ textDecoration: "none", color: "black" }}
-                    >
-                      <h4>
-                        <b>{item.name}</b>
-                      </h4>
-                    </Link>
-                    <p>
-                      {item.time.hour > 0 ? `${item.time.hour} hours` : null}{" "}
-                      {item.time.minutes > 0
-                        ? `${item.time.minutes} mins`
-                        : null}{" "}
-                    </p>
-                  </div>
+              <div key={item._id} className="card">
+                <img
+                  src={item.picture.avatar}
+                  alt="Avatar"
+                  style={{ width: "100%" }}
+                  className="landing-image"
+                />
+                <div className="card-text" key={item._id}>
+                  <Link
+                    to={`/recipes/${item._id}`}
+                    style={{ textDecoration: "none", color: "black" }}
+                  >
+                    <h4>
+                      <b>{item.name}</b>
+                    </h4>
+                  </Link>
+                  <p>
+                    {item.time.hour > 0 ? `${item.time.hour} hours` : null}{" "}
+                    {item.time.minutes > 0 ? `${item.time.minutes} mins` : null}{" "}
+                  </p>
                 </div>
-              </>
+              </div>
             );
           })}
         </div>
-        {/* <Link to={`/meals/breakfast`}>
-          <Button.Ghost
-            style={{
-              borderRadius: "5px",
-              padding: "10px 30px 10px 30px",
-              marginTop: "30px",
-              marginBottom: "100px",
-            }}
-          >
-            See More
-          </Button.Ghost>
-        </Link>
-
-        <h3 className="mealType">Lunch</h3>
-        <div className="container">
-          {lunch?.map((item) => {
-            return (
-              <>
-                <div key={item._id} className="card">
-                  <img
-                    src={item.picture}
-                    alt="Avatar"
-                    style={{ width: "100%" }}
-                    className="landing-image"
-                  />
-                  <div className="card-text" key={item._id}>
-                    <Link
-                      to={`/recipes/${item._id}`}
-                      style={{ textDecoration: "none", color: "black" }}
-                    >
-                      <h4>
-                        <b>{item.name}</b>
-                      </h4>
-                    </Link>
-                    <p>
-                      {item.time.hour > 0 ? `${item.time.hour} hours` : null}{" "}
-                      {item.time.minutes > 0
-                        ? `${item.time.minutes} mins`
-                        : null}{" "}
-                    </p>
-                  </div>
-                </div>
-              </>
-            );
-          })}
-        </div>
-        <Link to={`/meals/lunch`}>
-          <Button.Ghost
-            style={{
-              borderRadius: "5px",
-              padding: "10px 30px 10px 30px",
-              marginTop: "30px",
-              marginBottom: "100px",
-            }}
-          >
-            See More
-          </Button.Ghost>
-        </Link>
-
-        <h3 className="mealType">Dinner</h3>
-        <div className="container">
-          {dinner?.map((item) => {
-            return (
-              <>
-                <div key={item._id} className="card">
-                  <img
-                    src={item.picture}
-                    alt="Avatar"
-                    style={{ width: "100%" }}
-                    className="landing-image"
-                  />
-                  <div className="card-text" key={item._id}>
-                    <Link
-                      to={`/recipes/${item._id}`}
-                      style={{ textDecoration: "none", color: "black" }}
-                    >
-                      <h4>
-                        <b>{item.name}</b>
-                      </h4>
-                    </Link>
-                    <p>
-                      {item.time.hour > 0 ? `${item.time.hour} hours` : null}{" "}
-                      {item.time.minutes > 0
-                        ? `${item.time.minutes} mins`
-                        : null}{" "}
-                    </p>
-                  </div>
-                </div>
-              </>
-            );
-          })}
-        </div>
-        <Link to={`/meals/dinner`}>
-          <Button.Ghost
-            style={{
-              borderRadius: "5px",
-              padding: "10px 30px 10px 30px",
-              marginTop: "30px",
-              marginBottom: "100px",
-            }}
-          >
-            See More
-          </Button.Ghost>
-        </Link> */}
       </div>
     </>
   );
