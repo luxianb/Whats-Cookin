1f13911420c8d300c2a5a875efccb70c6c59c3e5
Perry-E <perryeber@gmail.com>
2021-11-03 19:32:19 +0800

    Changed routes for meal index

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,13 @@
 import { Route, Switch } from "react-router-dom";
-import "./App.css"
-import {useState, useEffect} from 'react';
+import "./App.css";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import LogIn from "./pages/LogIn";
-import NavigationBar from './components/NavigationBar'
-import Landing from "./pages/Landing/index"
-import CreateRecipe from "./pages/CreateRecipe/index"
+import NavigationBar from "./components/NavigationBar";
+import Landing from "./pages/Landing/index";
+import CreateRecipe from "./pages/CreateRecipe/index";
 import SignUp from "./pages/SignUp";
 import DisplayMeals from "./pages/DisplayMeals";
-import DisplayBreakfast from "./pages/DisplayMeals/BreakfastAll";
-import DisplayLunch from "./pages/DisplayMeals/LunchAll";
-import DisplayDinner from "./pages/DisplayMeals/DinnerAll";
-import SearchBar from "./components/SearchBar";
 
 function App() {
   const [userData, setUserData] = useState({});
@@ -21,44 +17,30 @@ function App() {
     async function fetchRecipeData() {
       const res = await axios.get("/api/recipes");
       setAllMeals(res.data);
-      console.log("res.data", res.data)
     }
     fetchRecipeData();
   }, []);
-
+  
   return (
-    <div style={{backgroundColor: 'white'}}>
-      <NavigationBar userData={userData}/>
+    <div style={{ backgroundColor: "white" }}>
+      <NavigationBar userData={userData} />
       <Switch>
         <Route exact path="/" component={Landing} />
 
         <Route exact path="/meals">
-          <DisplayMeals recipes={allMeals}/>
-          <SearchBar recipes={allMeals}/>
+          <DisplayMeals recipes={allMeals} />
         </Route>
-
-        {/* <Route exact path="/meals/breakfast">
-        <DisplayBreakfast recipes={card}/>
-        </Route>
-
-        <Route exact path="/meals/lunch">
-        <DisplayLunch recipes={card}/>
-        </Route>
-
-        <Route exact path="/meals/dinner">
-        <DisplayDinner recipes={card}/>
-        </Route> */}
 
         <Route exact path="/create" component={CreateRecipe} />
 
-        <Route path='/login'>
-          <LogIn onLoginSuccess={(userInfo) => setUserData(userInfo)}/>
+        <Route path="/login">
+          <LogIn onLoginSuccess={(userInfo) => setUserData(userInfo)} />
         </Route>
 
-        <Route path='/signup' component={SignUp}/>
+        <Route path="/signup" component={SignUp} />
       </Switch>
     </div>
-  )
+  );
 }
 
 export default App;
