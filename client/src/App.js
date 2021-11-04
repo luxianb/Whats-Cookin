831840667c2038a3336cb1ef78a466583cb89b11
aFar1s <89140460+aFar1s@users.noreply.github.com>
2021-11-04 14:49:21 +0800

    /app.js update fetch from dev

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,49 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import './App.css';
-import Landing from "./pages/Landing/index"
-import CreateRecipe from "./pages/CreateRecipe/index"
-
-// import NavigationBar from "../src/components/NavigationBar/index"
-
-
-
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import LogIn from "./pages/LogIn";
+import NavigationBar from "./components/NavigationBar";
+import Landing from "./pages/Landing/index";
+import CreateRecipe from "./pages/CreateRecipe/index";
+import SignUp from "./pages/SignUp";
+import UserProfile from "./pages/UserProfile";
+import RecipePage from "./pages/Recipe";
+import DisplayMeals from "./pages/DisplayMeals";
 
 function App() {
+  const [userData, setUserData] = useState({});
+  const [allMeals, setAllMeals] = useState();
 
+  useEffect(() => {
+    async function fetchRecipeData() {
+      const res = await axios.get("/api/recipes");
+      setAllMeals(res.data);
+    }
+    fetchRecipeData();
+  }, []);
+  
   return (
-    <Router>
-       {/* <NavigationBar /> */}
-     <div>
-       <Switch>
-         <Route exact path="/">
-          <Landing />
-         </Route>
-         <Route exact path="/create">
-           <CreateRecipe />
-         </Route>
+    <div style={{ backgroundColor: "white" }}>
+      <NavigationBar userData={userData} />
+      <Switch>
+        <Route exact path="/" component={Landing} />
 
-       </Switch>
-     </div>
-    </Router>
+        <Route exact path="/meals">
+          <DisplayMeals recipes={allMeals} />
+        </Route>
+
+        <Route exact path="/create" component={CreateRecipe} />
+
+        <Route path="/login">
+          <LogIn onLoginSuccess={(userInfo) => setUserData(userInfo)} />
+        </Route>
+
+        <Route path='/signup' component={SignUp}/>
+        <Route path='/profile/:userId' component={UserProfile}/>
+        <Route path='/recipe/:recipeId' component={RecipePage}/>
+      </Switch>
+    </div>
   );
 }
 
