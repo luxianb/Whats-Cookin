037f62e9ba950c7578943add3a2a66ab1f9503af
luxianb <40238895+luxianb@users.noreply.github.com>
2021-11-05 00:10:48 +0800

    Add link for meals page in profile

--- client/src/components/Cards/FindMealsPortal.jsx
+++ client/src/components/Cards/FindMealsPortal.jsx
@@ -5,7 +5,7 @@ export default function FindMealsPortal(props) {
   const history = useHistory();
 
   return(
-    <CardPortalBase onClick={() => history.push('#')} style={props.style}>
+    <CardPortalBase onClick={() => history.push('/meals')} style={props.style}>
       <i className="fa-solid fa-plus" style={{fontSize: '1.5rem'}}/>
       <h3>Find meals to add</h3>
     </CardPortalBase>
